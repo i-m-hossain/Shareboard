bc33f7670e44ea2050d4e8806909cb046796c5d9
Md. Imran Hossain <mdimran1409036@gmail.com>
2021-03-12 16:39:14 +0600

    create controller method added in bootstrap.php

--- classes/bootstrap.php
+++ classes/bootstrap.php
@@ -20,5 +20,33 @@ class Bootstrap
         } else {
             $this->action = $this->request['action'];
         }
+        
+    }
+    public function createController(){
+
+        //check class
+        if(class_exists($this->controller)){
+            $parents = class_parents($this->controller);              
+            //check extend class
+            if(in_array("Controller", $parents)){
+                if(method_exists($this->controller, $this->action)){
+                    return new $this->controller($this->action, $this->request);
+                }else{
+                    //method does not exist
+                    echo "<h1>Method does not exist</h1>";
+                    return;
+                }
+            }else{
+                // Base controller class does not exist
+                echo "<h1>Base controller(Parent) not found</h1>";
+                return;
+            }
+
+        }else{
+            // controller class does not exist
+            echo " <h1> Controller class does not exist </h1> ";
+            return;
+        }
+
     }
 }
